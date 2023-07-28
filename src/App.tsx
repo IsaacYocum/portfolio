6be03f6fc2b24ab553d1a03a570d27769181b5b7
isaac <isyocum@gmail.com>
2023-07-27 21:31:42 -0700

    Move navbar to it's own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
 import React, { FC } from 'react';
 import logo from './logo.svg';
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
 import { Box } from '@mui/system';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Outlet, Link } from "react-router-dom"
+import Navbar from './components/Navbar';
+import { Outlet } from "react-router-dom"
 import './App.css'
 
 type AppProps = { content?: React.ReactNode }
@@ -11,31 +10,7 @@ type AppProps = { content?: React.ReactNode }
 let App: FC<AppProps> = ({ content }) => {
   return (
     <Box sx={{ flexGrow: 1, height: '100%' }} >
-      <AppBar position="static">
-        <Toolbar variant="dense">
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">
-            <Link to={"/portfolio"}>Home</Link>
-          </Button>
-          <Button color="inherit">
-            <Link to={"about"}>About</Link>
-          </Button>
-          <Button color="inherit">
-            <Link to={"resume"}>Resume</Link>
-          </Button>
-        </Toolbar>
-      </AppBar>
+      <Navbar />
       <div id="content">
         <Outlet />
       </div>
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
+import { Link } from "react-router-dom"
+import MenuIcon from '@mui/icons-material/Menu';
+
+let Navbar = () => {
+    return (
+      <AppBar position="static" style={{marginBottom: '10px'}}>
+        <Toolbar variant="dense">
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">
+            <Link to={"/portfolio"}>Home</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to={"about"}>About</Link>
+          </Button>
+          <Button color="inherit">
+            <Link to={"resume"}>Resume</Link>
+          </Button>
+        </Toolbar>
+      </AppBar>
+    )
+}
+
+export default Navbar;
