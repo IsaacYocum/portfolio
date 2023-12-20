3dc38637a1388ad58ffb50d4af9f04c50ed45e12
isaac <isyocum@gmail.com>
2023-12-19 20:27:28 -0800

    footer center

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,24 +1,36 @@
 import { useTheme } from "@mui/material";
+import './Footer.css';
 
 let Footer = () => {
   let theme = useTheme();
+  console.log(theme)
 
   let styles = {
-    backgroundColor: theme.palette.background.paper
+    backgroundColor: theme.palette.primary.light,
   }
 
   return (
-    <footer style={styles}>
-      <div>Links</div>
-      <div>Site</div>
-      <div>
-        <ul>Contact info</ul>
-        <ul>email</ul>
-        <ul>github</ul>
-        <ul>linkedin</ul>
-        <ul>Facebook?</ul>
+    <footer
+      id="footer"
+      style={styles}
+    >
+      <div className="footerContent">
+        <div className="flexLinks">
+          Links
+          <ul>Home</ul>
+          <ul>About</ul>
+          <ul>Resume</ul>
+          <ul>Projects</ul>
+        </div>
+        <div className="flexLinks">
+          Contact
+          <ul>email</ul>
+          <ul>github</ul>
+          <ul>linkedin</ul>
+          <ul>Facebook?</ul>
+        </div>
+        <div className="flexFiller">Projects</div>
       </div>
-      <div>Projects</div>
     </footer>
   )
 }
