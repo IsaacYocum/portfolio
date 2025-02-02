f1c4c43a9c0439225a56821b9f5caa040369bc26
isaac <isyocum@gmail.com>
2025-02-02 13:44:18 -0800

    wip - figuring out phase transitions

--- src/components/projects/pomodoro/pomodoro.tsx
+++ src/components/projects/pomodoro/pomodoro.tsx
@@ -1,71 +1,122 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './pomodoro.css';
 
 const Pomodoro = () => {
-    const enum STATES {
+    const enum RUNNING_STATES {
+        RESET,
         RUNNING,
         PAUSED,
-        STOPPED,
+        FINISHED,
+    }
+
+    const enum PHASE_STATES {
+        POMODORO,
         SHORT_BREAK,
         LONG_BREAK, 
     }
 
-    const runLength = 25 * 60
+    const POMODORO_RUN_LENGTH_SECS = 3 
+    const SHORT_BREAK_RUN_LENGTH_SECS = 3 
+    const LONG_BREAK_RUN_LENGTH_SECS = 3 
 
-    const [state, setState] = useState(STATES.STOPPED)
-    const [running, setRunning] = useState(false);
-    const [minutes, setMinutes] = useState(25)
-    const [seconds, setSeconds] = useState(0)
-    const [timerDisplay, setTimerDisplay] = useState("25:00")
-    const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | undefined>(undefined)
+    const [runLengthSecs, setRunLengthSecs] = useState(POMODORO_RUN_LENGTH_SECS)
 
-    let timer: NodeJS.Timer;
-
-    const calcDisplay = (currSecs: number) => {
-        const remainingSecs = runLength - currSecs
+    const calcDisplay = useCallback((currSecs: number) => {
+        const remainingSecs = runLengthSecs - currSecs
         const mins = Math.floor(remainingSecs / 60);
         const secs = remainingSecs % 60;
         const displayMins = mins < 10 ? `0${mins}` : mins;
         const displaySecs = secs < 10 ? `0${secs}` : secs;
         
         return `${(displayMins)}:${displaySecs}`
-    }
+    }, [runLengthSecs])
 
-    const handleStart = () => {
-        setRunning(true)
+    const [runningState, setRunningState] = useState(RUNNING_STATES.RESET)
+    const [phaseState, setPhaseState] = useState(PHASE_STATES.POMODORO)
+    const [numPomodoros, setNumPomodoros] = useState(0)
+    const [runTimeSecs, setRunTimeSecs] = useState(0)
+    const [timerDisplay, setTimerDisplay] = useState(calcDisplay(0))
+    const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | undefined>(undefined)
+
+    const stopRunning = useCallback(() => {
+        clearInterval(timerInterval)
+        setTimerInterval(undefined)
+    }, [timerInterval])
+
+    useEffect(() => {
+        if (runTimeSecs >= runLengthSecs) {
+            setRunningState(RUNNING_STATES.FINISHED)
+            // stopRunning()
+        }
+    }, [runTimeSecs, stopRunning, RUNNING_STATES.FINISHED, runLengthSecs])
+
+    
+    const handleStart = useCallback(() => {
+        setRunningState(RUNNING_STATES.RUNNING)
+
+        if (timerInterval) {
+            clearInterval(timerInterval)
+        }
 
         setTimerInterval( 
             setInterval(() => {
-                setSeconds(prev => {
-                    let currSecs = prev += 1
+                setRunTimeSecs(prev => {
+                    const currSecs = prev += 1
                     console.log(calcDisplay(currSecs))
                     setTimerDisplay(calcDisplay(currSecs))
-                    if (currSecs === runLength) {
-                        stopRunning()
-                    }
+                    console.log(currSecs, runLengthSecs)
                     return currSecs
                 })
             }, 1000)
         )
-    }
+    }, [RUNNING_STATES.RUNNING, calcDisplay, runLengthSecs, timerInterval])
 
-    const stopRunning = () => {
-        clearInterval(timerInterval)
-        setRunning(false)
-        setTimerInterval(undefined)
-    }
 
-    const handlePause = () => {
+    // function changePhase(phase: PHASE_STATES, runLengthSecs: number) {
+    const changePhase = useCallback((phase: PHASE_STATES, runLengthSecs: number) => {
+        stopRunning()
+        setRunLengthSecs(runLengthSecs)
+        setRunTimeSecs(0)
+        setPhaseState(phase)
+        // handleStart()
+    }, [handleStart, stopRunning])
+
+    useEffect(() => {
+        if (runningState === RUNNING_STATES.FINISHED) {
+            clearInterval(timerInterval)
+            switch(phaseState) {
+                case PHASE_STATES.POMODORO:
+                    setNumPomodoros(prev => {
+                        const timesRun = prev + 1;
+                        if (timesRun % 4 !== 0) {
+                            changePhase(PHASE_STATES.SHORT_BREAK, SHORT_BREAK_RUN_LENGTH_SECS)
+                        } else {
+                            changePhase(PHASE_STATES.LONG_BREAK, LONG_BREAK_RUN_LENGTH_SECS)
+                        } 
+
+                        return timesRun;
+                    })
+                    break;
+                case PHASE_STATES.SHORT_BREAK:
+                case PHASE_STATES.LONG_BREAK:
+                    changePhase(PHASE_STATES.POMODORO, POMODORO_RUN_LENGTH_SECS)
+                    break;
+            }
+        }
+    }, [runningState, phaseState, PHASE_STATES.LONG_BREAK, PHASE_STATES.POMODORO, PHASE_STATES.SHORT_BREAK, RUNNING_STATES.FINISHED, RUNNING_STATES.RUNNING, changePhase, timerInterval])
+
+    function handlePause() {
+        setRunningState(RUNNING_STATES.PAUSED)
         stopRunning()
     }
 
-    const resetTimer = () => {
-        setMinutes(runLength / 60);
-        setSeconds(0);
+    function resetTimer() {
+        setRunTimeSecs(0);
         setTimerDisplay(calcDisplay(0))
     }
 
-    const handleStop = () => {
+    function handleReset() {
+        setRunningState(RUNNING_STATES.RESET)
         stopRunning()
         resetTimer()
     }
@@ -73,30 +124,35 @@ const Pomodoro = () => {
     return (
         <div className="pomodoro">
             <div className='headers centered'>
-                <p>Pomodoro</p>
-                <p>Short Break</p>
-                <p>Long Break</p>
+                <p
+                    style={{ backgroundColor: phaseState === PHASE_STATES.POMODORO ? 'pink' : "" }}
+                >Pomodoro</p>
+                <p
+                    style={{ backgroundColor: phaseState === PHASE_STATES.SHORT_BREAK ? 'pink' : "" }}
+                >Short Break</p>
+                <p
+                    style={{ backgroundColor: phaseState === PHASE_STATES.LONG_BREAK ? 'pink' : "" }}
+                >Long Break</p>
             </div>
             <div className='timer centered'>
                 <h1>{timerDisplay}</h1>
             </div>
-            <div className='buttons centered'>
+            <div className='buttons centered' >
                 <button 
-                    disabled={running} 
+                    disabled={runningState !== RUNNING_STATES.RESET && runningState !== RUNNING_STATES.PAUSED} 
                     onClick={handleStart}>
                     Start
                 </button>
                 <button 
-                    disabled={!running}
+                    disabled={runningState !== RUNNING_STATES.RUNNING && runningState !== RUNNING_STATES.FINISHED}
                     onClick={handlePause}>
                     Pause 
                 </button>
                 <button 
-                    onClick={handleStop}>
-                    Stop
+                    onClick={handleReset}>
+                    Reset
                 </button>
             </div>
-
         </div>
     )
 }
