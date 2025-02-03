4ba485bbec1e4810bcca1ac288a1bae6e86d0b5e
IsaacYocum <isyocum@gmail.com>
2025-02-02 22:06:23 -0800

    wip - isses with phase transition

--- src/components/projects/pomodoro/pomodoro.tsx
+++ src/components/projects/pomodoro/pomodoro.tsx
@@ -1,90 +1,59 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import './pomodoro.css';
 
+const enum RUNNING_STATES {
+    RESET,
+    RUNNING,
+    PAUSED,
+    FINISHED,
+}
+
+const enum PHASE_STATES {
+    POMODORO,
+    SHORT_BREAK,
+    LONG_BREAK,
+}
+
+const POMODORO_RUN_LENGTH_SECS = 2
+const SHORT_BREAK_RUN_LENGTH_SECS = 1
+const LONG_BREAK_RUN_LENGTH_SECS = 3 
+
 const Pomodoro = () => {
-    const enum RUNNING_STATES {
-        RESET,
-        RUNNING,
-        PAUSED,
-        FINISHED,
-    }
-
-    const enum PHASE_STATES {
-        POMODORO,
-        SHORT_BREAK,
-        LONG_BREAK, 
-    }
-
-    const POMODORO_RUN_LENGTH_SECS = 3 
-    const SHORT_BREAK_RUN_LENGTH_SECS = 3 
-    const LONG_BREAK_RUN_LENGTH_SECS = 3 
-
-    const [runLengthSecs, setRunLengthSecs] = useState(POMODORO_RUN_LENGTH_SECS)
-
-    const calcDisplay = useCallback((currSecs: number) => {
-        const remainingSecs = runLengthSecs - currSecs
+    const calcDisplay = (currSecs: number, runLength: number) => {
+        console.log('calcDisplay')
+        const remainingSecs = runLength - currSecs
         const mins = Math.floor(remainingSecs / 60);
         const secs = remainingSecs % 60;
         const displayMins = mins < 10 ? `0${mins}` : mins;
         const displaySecs = secs < 10 ? `0${secs}` : secs;
         
         return `${(displayMins)}:${displaySecs}`
-    }, [runLengthSecs])
+    }
 
+    const [runLengthSecs, setRunLengthSecs] = useState(POMODORO_RUN_LENGTH_SECS)
     const [runningState, setRunningState] = useState(RUNNING_STATES.RESET)
     const [phaseState, setPhaseState] = useState(PHASE_STATES.POMODORO)
     const [numPomodoros, setNumPomodoros] = useState(0)
     const [runTimeSecs, setRunTimeSecs] = useState(0)
-    const [timerDisplay, setTimerDisplay] = useState(calcDisplay(0))
-    const [timerInterval, setTimerInterval] = useState<NodeJS.Timer | undefined>(undefined)
+    const [timerDisplay, setTimerDisplay] = useState(calcDisplay(0, POMODORO_RUN_LENGTH_SECS))
+    const timerInterval = useRef<NodeJS.Timer>()
 
     const stopRunning = useCallback(() => {
-        clearInterval(timerInterval)
-        setTimerInterval(undefined)
+        setRunningState(RUNNING_STATES.RESET)
+        clearInterval(timerInterval.current)
     }, [timerInterval])
 
     useEffect(() => {
-        if (runTimeSecs >= runLengthSecs) {
-            setRunningState(RUNNING_STATES.FINISHED)
-            // stopRunning()
-        }
-    }, [runTimeSecs, stopRunning, RUNNING_STATES.FINISHED, runLengthSecs])
+        const updatePhaseState = () => {
+            const changePhase = (phase: PHASE_STATES, runLengthSecs: number) => {
+                // stopRunning()
+                setRunLengthSecs(runLengthSecs)
+                setRunTimeSecs(0)
+                setPhaseState(phase)
+                // handleStart()
+            }
 
-    
-    const handleStart = useCallback(() => {
-        setRunningState(RUNNING_STATES.RUNNING)
-
-        if (timerInterval) {
-            clearInterval(timerInterval)
-        }
-
-        setTimerInterval( 
-            setInterval(() => {
-                setRunTimeSecs(prev => {
-                    const currSecs = prev += 1
-                    console.log(calcDisplay(currSecs))
-                    setTimerDisplay(calcDisplay(currSecs))
-                    console.log(currSecs, runLengthSecs)
-                    return currSecs
-                })
-            }, 1000)
-        )
-    }, [RUNNING_STATES.RUNNING, calcDisplay, runLengthSecs, timerInterval])
-
-
-    // function changePhase(phase: PHASE_STATES, runLengthSecs: number) {
-    const changePhase = useCallback((phase: PHASE_STATES, runLengthSecs: number) => {
-        stopRunning()
-        setRunLengthSecs(runLengthSecs)
-        setRunTimeSecs(0)
-        setPhaseState(phase)
-        // handleStart()
-    }, [handleStart, stopRunning])
-
-    useEffect(() => {
-        if (runningState === RUNNING_STATES.FINISHED) {
-            clearInterval(timerInterval)
-            switch(phaseState) {
+            switch (phaseState) {
                 case PHASE_STATES.POMODORO:
                     setNumPomodoros(prev => {
                         const timesRun = prev + 1;
@@ -92,7 +61,7 @@ const Pomodoro = () => {
                             changePhase(PHASE_STATES.SHORT_BREAK, SHORT_BREAK_RUN_LENGTH_SECS)
                         } else {
                             changePhase(PHASE_STATES.LONG_BREAK, LONG_BREAK_RUN_LENGTH_SECS)
-                        } 
+                        }
 
                         return timesRun;
                     })
@@ -103,7 +72,31 @@ const Pomodoro = () => {
                     break;
             }
         }
-    }, [runningState, phaseState, PHASE_STATES.LONG_BREAK, PHASE_STATES.POMODORO, PHASE_STATES.SHORT_BREAK, RUNNING_STATES.FINISHED, RUNNING_STATES.RUNNING, changePhase, timerInterval])
+
+        if (runTimeSecs >= runLengthSecs) {
+            updatePhaseState()
+        }
+    }, [runTimeSecs, stopRunning, runLengthSecs, phaseState])
+
+    const handleStart = useCallback(() => {
+        setRunningState(RUNNING_STATES.RUNNING)
+
+        timerInterval.current = setInterval(() => {
+            setRunTimeSecs(prev => {
+                const currSecs = prev + 1
+                setTimerDisplay(calcDisplay(currSecs, runLengthSecs))
+                return currSecs
+            })
+        }, 1000)
+    }, [runLengthSecs])
+
+
+    // useEffect(() => {
+
+    //     if (runningState === RUNNING_STATES.FINISHED) {
+    //         updatePhaseState();
+    //     }
+    // }, [runningState, handleStart, phaseState, stopRunning])
 
     function handlePause() {
         setRunningState(RUNNING_STATES.PAUSED)
@@ -112,7 +105,7 @@ const Pomodoro = () => {
 
     function resetTimer() {
         setRunTimeSecs(0);
-        setTimerDisplay(calcDisplay(0))
+        setTimerDisplay(calcDisplay(0, runLengthSecs))
     }
 
     function handleReset() {
