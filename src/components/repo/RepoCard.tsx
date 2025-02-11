dbc85eb0b7847cbbe8129770e7d9ea838990ac9a
IsaacYocum <isyocum@gmail.com>
2025-02-11 15:18:09 -0800

    repo card hover and click

--- src/components/repo/RepoCard.tsx
+++ src/components/repo/RepoCard.tsx
@@ -1,30 +1,31 @@
 import GitHubIcon from '@mui/icons-material/GitHub';
-import { IconButton, styled } from "@mui/material";
-import { FC } from "react";
+import { IconButton, styled } from '@mui/material';
+import { FC } from 'react';
 
 const Card = styled('div')(({ theme }) => ({
-  overflowY: "auto",
-  marginRight: "10px",
-  marginBottom: "10px",
-  wordBreak: "break-word",
-  maxWidth: "250px",
-  minHeight: "120px",
-  maxHeight: "150px",
-  border: "1px solid",
-  display: "grid",
-  gridTemplateRows: "1fr min-content",
-  gridTemplateAreas: "'nameAndDescription'\n    'languageAndLink'",
+  overflowY: 'auto',
+  marginRight: '10px',
+  marginBottom: '10px',
+  wordBreak: 'break-word',
+  maxWidth: '250px',
+  minHeight: '120px',
+  maxHeight: '150px',
+  border: '1px solid',
+  display: 'grid',
+  gridTemplateRows: '1fr min-content',
+  gridTemplateAreas: "'nameAndDescription'\n 'languageAndLink'",
   backgroundColor: theme.palette.background.default,
   '&:hover': {
-    backgroundColor: theme.palette.background.paper
-  }
-}))
+    backgroundColor: theme.palette.background.paper,
+    cursor: 'pointer',
+  },
+}));
 
 const NameAndDescription = styled('div')(({ theme }) => ({
   gridArea: 'nameAndDescription',
   color: theme.palette.text.header,
   backgroundColor: theme.palette.background.header,
-}))
+}));
 
 const Name = styled('p')({
   textAlign: 'center',
@@ -32,40 +33,40 @@ const Name = styled('p')({
   margin: 0,
   paddingLeft: '5px',
   paddingRight: '5px',
-})
+});
 
 const Description = styled('p')({
   margin: 0,
   paddingLeft: '5px',
   paddingRight: '5px',
-})
+});
 
 const LanguageAndLink = styled('div')({
-  display: "grid",
-  gridArea: "languageAndLink",
+  display: 'grid',
+  gridArea: 'languageAndLink',
   gridTemplateAreas: "'language ghLink'",
-  alignItems: "center",
-  justifyItems: "center",
-  borderTop: "1px solid"
-})
+  alignItems: 'center',
+  justifyItems: 'center',
+  borderTop: '1px solid',
+});
 
 const Language = styled('div')({
-  gridArea: 'language'
-})
+  gridArea: 'language',
+});
 
 const Link = styled('div')({
-  gridArea: 'ghLink'
-})
+  gridArea: 'ghLink',
+});
 
-type RepoCardProps = { repo: any }
+type RepoCardProps = { repo: any };
 
 const RepoCard: FC<RepoCardProps> = ({ repo }) => {
   const handleOnClick = () => {
     window.open(repo.html_url, '_blank');
-  }
+  };
 
   return (
-    <Card key={repo.name}>
+    <Card key={repo.name} onClick={handleOnClick}>
       <NameAndDescription>
         <Name>{repo.name}</Name>
         <Description>{repo.description}</Description>
@@ -79,7 +80,7 @@ const RepoCard: FC<RepoCardProps> = ({ repo }) => {
         </Link>
       </LanguageAndLink>
     </Card>
-  )
-}
+  );
+};
 
 export default RepoCard;
