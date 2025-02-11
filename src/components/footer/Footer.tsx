d98110f777ce21317fe1c7256bf7283ed061899f
IsaacYocum <isyocum@gmail.com>
2025-02-11 11:31:00 -0800

    update to use mui styled components

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,61 +1,94 @@
-import { useTheme } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { Typography, Link as LinkBase } from '@mui/material';
-import { FC } from "react";
+import { FC, useMemo } from "react";
 import { Link } from "react-router-dom";
 import { VISIBLE_LINKS } from "../../routes/routes";
-import './Footer.css';
+
+const FooterParent = styled('footer')({
+  display: 'flex',
+  justifyContent: 'center',
+  zIndex: 2,
+  minHeight: '250px',
+  marginLeft: '20px',
+  marginRight: '20px',
+  "@media screen and (max-width: 30em)": {
+    "#footerProjects": { display: "none" }
+  }
+})
+
+const FooterContent = styled('footer')({
+  display: "flex",
+  flexDirection: "row",
+  maxWidth: "1280px",
+  gap: "5em",
+  paddingLeft: "20px",
+  paddingRight: "20px"
+})
+
+const FooterColumn = styled('div')({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '10px'
+})
+
+const FooterHeader = styled('p')({
+  textTransform: 'uppercase',
+  opacity: '80%'
+})
+
+const RepoListContainer = styled('div')({
+  overflowY: "scroll", 
+  maxHeight: "250px"
+})
 
 type FooterProps = { repoViewer?: React.ReactNode | null }
 
-let Footer: FC<FooterProps> = ({ repoViewer }) => {
-  let theme = useTheme();
+const Footer: FC<FooterProps> = ({ repoViewer }) => {
+  const SiteLinks = useMemo(() => {
+    return VISIBLE_LINKS.map(route => (
+      <LinkBase
+        key={route.title}
+        to={route.path}
+        component={Link}
+      >
+        <Typography>{route.title}</Typography>
+      </LinkBase>
+    ))
+  }, [])
 
-  let styles = {
-    backgroundColor: theme.palette.background.footer,
-    color: theme.palette.text.footer,
-  }
-
-  let linkStyles = {
-    color: theme.palette.text.footer,
-  }
+  const ContactLinks = useMemo(() => (
+      <>
+        <LinkBase href="mailto:isyocum@gmail.com">
+          <Typography>Email</Typography>
+        </LinkBase>
+        <LinkBase href="https://github.com/IsaacYocum" target='_blank'>
+          <Typography>GitHub</Typography>
+        </LinkBase>
+        <LinkBase href="https://www.linkedin.com/in/isaacyocum/" target='_blank'>
+          <Typography>LinkedIn</Typography>
+        </LinkBase>
+      </>
+  ), [])
 
   return (
-    <footer
-      id="footer"
-      style={styles}
-    >
-      <div className="footerContent">
-        <div className="flexLinks">
-          <p className='footerHeader'>Site</p>
-          {VISIBLE_LINKS.map(route => (
-            <div
-              key={route.title}
-              className="linkItem"
-            >
-              <LinkBase
-                style={linkStyles}
-                to={route.path}
-                component={Link}
-              >
-                <Typography>{route.title}</Typography>
-              </LinkBase>
-            </div>
-          ))}
-        </div>
-        <div className="flexLinks">
-          <p className='footerHeader'>Contact</p>
-          <p><LinkBase style={linkStyles} href="mailto:isyocum@gmail.com">Email</LinkBase></p>
-          <p><LinkBase style={linkStyles} href="https://github.com/IsaacYocum" target='_blank'>GitHub</LinkBase></p>
-          <p><LinkBase style={linkStyles} href="https://www.linkedin.com/in/isaacyocum/" target='_blank'>LinkedIn</LinkBase></p>
-        </div>
-        <div id="footerProjects">
-          <p className='footerHeader'>Projects</p>
-          <div className="footerRepoList">
+    <FooterParent>
+      <FooterContent>
+        <FooterColumn>
+          <FooterHeader>Site</FooterHeader>
+          {SiteLinks}
+        </FooterColumn>
+        <FooterColumn>
+          <FooterHeader>Contact</FooterHeader>
+          {ContactLinks}
+        </FooterColumn>
+        <FooterColumn id="footerProjects">
+          <FooterHeader>Projects</FooterHeader>
+          <RepoListContainer>
             {repoViewer}
-          </div>
-        </div>
-      </div>
-    </footer>
+          </RepoListContainer>
+        </FooterColumn>
+      </FooterContent>
+    </FooterParent>
   )
 }
 
