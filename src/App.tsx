8b35ae3eb6d34f23b4699893b485d55fb5936036
IsaacYocum <isyocum@gmail.com>
2025-02-11 14:40:24 -0800

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { CssBaseline } from '@mui/material';
 import { styled, Theme, ThemeProvider } from '@mui/material/styles';
 import React, { FC, useMemo, useState } from 'react';
-import { useQuery } from "react-query";
-import { Outlet, useOutletContext } from "react-router-dom";
+import { useQuery } from 'react-query';
+import { Outlet, useOutletContext } from 'react-router-dom';
 import Themes from './Themes';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
@@ -10,15 +10,15 @@ import RepoViewer from './components/repo/RepoViewer';
 import { GITHUB_REPOS_URL, REPO_NAMES_TO_DISPLAY } from './constants';
 
 const Content = styled('div')({
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  maxWidth: "1280px",
-  margin: "2em",
-  gap: "15px"
-})
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  maxWidth: '1280px',
+  margin: '2em',
+  gap: '15px',
+});
 
-type ContextType = { repoViewer: React.ReactNode | null }
+type ContextType = { repoViewer: React.ReactNode | null };
 
 export function useRepoViewer() {
   return useOutletContext<ContextType>();
@@ -27,29 +27,29 @@ export function useRepoViewer() {
 const fetchGitHubRepos = async () => {
   const res = await fetch(GITHUB_REPOS_URL);
   const ghRepos: Array<any> = await res.json();
-  return ghRepos.filter((repo: any) => REPO_NAMES_TO_DISPLAY.includes(repo.name));
+  return ghRepos.filter((repo: any) =>
+    REPO_NAMES_TO_DISPLAY.includes(repo.name)
+  );
 };
 
-type AppProps = { content?: React.ReactNode }
+type AppProps = { content?: React.ReactNode };
 
 const App: FC<AppProps> = () => {
   const [theme, setTheme] = useState<Theme>(Themes[1]);
-  const { data: ghRepos } = useQuery("ghRepos", fetchGitHubRepos);
+  const { data: ghRepos } = useQuery('ghRepos', fetchGitHubRepos);
 
-  const repoViewer = useMemo(() => (
-    <RepoViewer repos={ghRepos} />
-  ), [ghRepos])
+  const repoViewer = useMemo(() => <RepoViewer repos={ghRepos} />, [ghRepos]);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header onThemeChange={theme => setTheme(theme)} />
+      <Header onThemeChange={(theme) => setTheme(theme)} />
       <Content id="content">
         <Outlet context={{ repoViewer }} />
       </Content>
       <Footer repoViewer={repoViewer} />
-    </ThemeProvider >
+    </ThemeProvider>
   );
-}
+};
 
 export default App;
