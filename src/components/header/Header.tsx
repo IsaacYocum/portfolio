20f75ce00c32ea0edbc0df2578e97c01d50dad72
IsaacYocum <isyocum@gmail.com>
2025-02-13 10:31:42 -0800

    use styled components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,34 @@
 import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, Button, Divider, Drawer, FormControl, Icon, IconButton, InputLabel, Link as LinkBase, MenuItem, Select, SelectChangeEvent, styled, Theme, Toolbar, Typography, useTheme } from '@mui/material';
+import {
+  AppBar,
+  Button,
+  Divider,
+  Drawer,
+  FormControl,
+  Icon,
+  IconButton,
+  InputLabel,
+  Link as LinkBase,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  styled,
+  Theme,
+  Toolbar,
+  Typography,
+  useTheme,
+} from '@mui/material';
 import { FC, ReactNode, useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Themes, { ThemeName } from '../../Themes';
 import drawerIcon from '../../assets/drawer.svg';
 import { VISIBLE_LINKS } from '../../routes/routes';
-import './Header.css';
+
+const DrawerContainer = styled('div')({
+  '@media screen and (max-width: 30em)': {
+    '#appbarLinks': { display: 'none' },
+  },
+});
 
 const DrawerContents = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.background.default,
@@ -14,44 +37,46 @@ const DrawerContents = styled('div')(({ theme }) => ({
   flexGrow: 1,
   paddingLeft: 20,
   paddingRight: 20,
-  zIndex: 3
-}))
+  zIndex: 3,
+}));
+
+const ThemeSelectorContainer = styled('div')({
+  marginTop: 'auto',
+  textAlign: 'center',
+  marginBottom: '10px',
+});
 
 interface AppProps {
   onThemeChange: (theme: Theme) => void;
 }
 
 const Header: FC<AppProps> = ({ onThemeChange }) => {
-  let theme = useTheme()
+  const theme = useTheme();
 
-  let [title, setTitle] = useState('Home')
-  let [drawerOpen, setDrawerOpen] = useState(false)
+  const [title, setTitle] = useState('Home');
+  const [drawerOpen, setDrawerOpen] = useState(false);
 
-  let clickLink = (title: string, fromDrawer: boolean) => {
+  const clickLink = (title: string, fromDrawer: boolean) => {
     if (fromDrawer) setDrawerOpen(false);
     setTitle(title);
-  }
+  };
 
-  let toggleDrawer = (open: boolean) => {
+  const toggleDrawer = (open: boolean) => {
     setDrawerOpen(open);
-  }
+  };
 
-  let handleThemeChange = (event: SelectChangeEvent<string>) => {
-    let theme = Themes.find(theme => theme.id === event.target.value);
+  const handleThemeChange = (event: SelectChangeEvent<string>) => {
+    const theme = Themes.find((theme) => theme.id === event.target.value);
     if (theme) onThemeChange(theme);
-  }
+  };
 
   function createLinks(fromDrawer = false) {
-
-    return VISIBLE_LINKS.map(route => (
+    return VISIBLE_LINKS.map((route) => (
       <Button
         key={route.path}
         onClick={() => clickLink(route.title, fromDrawer)}
       >
-        <LinkBase
-          to={route.path}
-          component={Link}
-        >
+        <LinkBase to={route.path} component={Link}>
           {route.title}
         </LinkBase>
       </Button>
@@ -60,9 +85,15 @@ const Header: FC<AppProps> = ({ onThemeChange }) => {
 
   const memeDrawerIcon = (
     <Icon>
-      <img src={drawerIcon} alt='Pixel Road' height={20} width={28} style={{ transform: drawerOpen ? 'scale(-1, -1)' : 'none' }} />
+      <img
+        src={drawerIcon}
+        alt="Pixel Road"
+        height={20}
+        width={28}
+        style={{ transform: drawerOpen ? 'scale(-1, -1)' : 'none' }}
+      />
     </Icon>
-  )
+  );
 
   const createAppBar = (openDrawer: boolean, children?: ReactNode[]) => {
     return (
@@ -79,47 +110,46 @@ const Header: FC<AppProps> = ({ onThemeChange }) => {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {title}
           </Typography>
-          <div id='appbarLinks'>
-            {children}
-          </div>
+          <div id="appbarLinks">{children}</div>
         </Toolbar>
       </AppBar>
-    )
-  }
+    );
+  };
 
   const drawer = (
-    <Drawer
-      open={drawerOpen}
-      onClose={() => toggleDrawer(false)}
-    >
+    <Drawer open={drawerOpen} onClose={() => toggleDrawer(false)}>
       {createAppBar(false)}
       <DrawerContents>
         {createLinks(true)}
-        <div style={{ marginTop: 'auto', textAlign: 'center', marginBottom: '10px' }}>
+        <ThemeSelectorContainer>
           <Divider />
           <FormControl fullWidth>
             <InputLabel id="themeSelector">Theme</InputLabel>
             <Select
-              id='themeSelector'
-              labelId='themeSelector'
-              label='Theme'
+              id="themeSelector"
+              labelId="themeSelector"
+              label="Theme"
               value={theme.id}
               onChange={handleThemeChange}
             >
-              {Themes.map(theme => <MenuItem key={theme.id} value={theme.id}>{theme.id}</MenuItem>)}
+              {Themes.map((theme) => (
+                <MenuItem key={theme.id} value={theme.id}>
+                  {theme.id}
+                </MenuItem>
+              ))}
             </Select>
           </FormControl>
-        </div>
+        </ThemeSelectorContainer>
       </DrawerContents>
     </Drawer>
-  )
+  );
 
   return (
-    <>
+    <DrawerContainer>
       {drawer}
       {createAppBar(true, createLinks())}
-    </>
-  )
-}
+    </DrawerContainer>
+  );
+};
 
 export default Header;
